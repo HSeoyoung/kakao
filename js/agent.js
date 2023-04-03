47c32cdc82ef4c0fb98e64a9cff07938fbee1635
HSeoyoung <121008943+HSeoyoung@users.noreply.github.com>
2023-04-03 16:48:20 +0900

    Delete agent.js

--- js/agent.js
+++
@@ -1,9 +0,0 @@
-let smartPhones=['iphone','ipod','ipad','android','opera mini','opera mobi','nokia',
-'webos','windows ce','blackberry','iemobile','sonyericssion'];
-
-//스마트폰 구분
-for(let i in smartPhones) {
-    if(navigator.userAgent.toLowerCase().match(new RegExp(smartPhones[i]))) {
-        document.location='http://hs026.dothome.co.kr/mindex.html';
-    }
-}
